61ffc6a4dddcf31cf1b9ab417fd925ef81566aa6
David <davidshinlee13@gmail.com>
2023-03-10 16:00:23 -0500

    lab4partial

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const port = 8080;
 const logger = require("morgan");
 const db = require('./db/db_connection');
 
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
+
 app.use(logger("dev"));
 
 // define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
+// define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
-} );
+    res.render('index');
+});
 
 const read_stuff_all_sql = `
     SELECT 
@@ -25,13 +30,32 @@ app.get( "/stuff", ( req, res ) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else
-            res.send(results);
+            res.send(results[0]); // results is still an array
     });
 });
 
-app.get( "/stuff/item", ( req, res ) => {
-    res.sendFile( __dirname + "/views/item.html" );
-} );
+// define a route for the item detail page
+const read_item_sql = `
+    SELECT 
+        item, quantity, description 
+    FROM
+        stuff
+    WHERE
+        id = ?
+`
+// define a route for the item detail page
+app.get( "/stuff/item/:id", ( req, res ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); 
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = "${req.params.id}"` ); 
+        else {
+            let data = results[0]; 
+            res.render('item', data);
+        }
+    });
+});
 
 // start the server
 app.listen( port, () => {
